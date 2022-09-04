f723c96595d5d901e2a7f43e5600dda2ae8e43f2
facundo2309 <facundoomargarcia@gmail.com>
2022-09-04 18:25:06 -0300

    update

--- app.js
+++ app.js
@@ -1,99 +1,123 @@
 
-    const game={
-        words:['ALURA','BINGO','ORACLE','UNO','ARGENTINA'],
-        // words:['ARGENTINA'],
-        estate:[0,1,2,3,4,5,6],
-        winWord:[],
-        winLeters:[],
-        // winLetersHistory:[],
-        wrongLeters:[],
-        keyPress:''
-    }
-    const htmlElemnts={
-        estateImage:document.getElementById('img'),
-        listWinLeters: document.getElementsByClassName('check-word'),
-        listWrongLeters: document.getElementsByClassName('wrong-word'),
-    }
-    function updateEstateGame(estate=0){
-        htmlElemnts.estateImage.src='assets/img/estates/e'+estate+'.png'
-        return estate
-    }
-    function choiceRandomWord(){
-        const min=0
-        const max=game.words.length-1
-        const randomIndex= Math.round(Math.random() * (max - min)) + min
-        game.winWord=game.words[randomIndex].split('')
-        return game.winWord
-    }
-    function updateLeters(leter){
-        if(game.winWord.includes(leter)){
+const game={
+    words:['ALURA','BINGO','ORACLE','UNO','ARGENTINA'],
+    // words:['ARGENTINA'],
+    estate:0,
+    winWord:[],
+    winLeters:[],
+    // winLetersHistory:[],
+    wrongLeters:[],
+    keyPress:''
+}
+const htmlElemnts={
+    estateImage:document.getElementById('img'),
+    listWinLeters: document.getElementsByClassName('check-word'),
+    listWrongLeters: document.getElementsByClassName('wrong-word'),
+}
+function updateEstateGame(estate=0){
+    htmlElemnts.estateImage.src='assets/img/estates/e'+estate+'.png'
+    return estate
+}
+function choiceRandomWord(){
+    const min=0
+    const max=game.words.length-1
+    const randomIndex= Math.round(Math.random() * (max - min)) + min
+    game.winWord=game.words[randomIndex].split('')
+    createSpaceWord(game.winWord)
+    return game.winWord
+}
+function updateLeters(leter){
+    if(game.winWord.includes(leter)){
             
 
-            for(let i=0; i< game.winWord.length;i++){
+        for(let i=0; i< game.winWord.length;i++){
                 
-                if(game.winWord[i] === leter ){
-                    
-                    // if(!game.winLetersHistory.includes(leter) || !game.winLetersHistory.includes(i)){
-                    //     game.winLeters=[...game.winLeters,{leter,i}]
-                    //     game.winLetersHistory.push(leter,i)
-                        
-                    // }
-                    if(!game.winLeters.filter(e => e.leter === leter).length > 0 ||
-                        !game.winLeters.filter(e => e.i === i).length > 0
-                    ){
-                        game.winLeters=[...game.winLeters,{leter,i}]
+            if(game.winWord[i] === leter ){
+   
+                if(!game.winLeters.filter(e => e.leter === leter).length > 0 ||
+                    !game.winLeters.filter(e => e.i === i).length > 0
+                ){
+                    game.winLeters=[...game.winLeters,{leter,i}]
                         
                         
-                    }
-                    
                 }
+                    
             }
+        }
             
-            return game
+        return 
+    }
+    if(!game.wrongLeters.includes(leter)){
+        game.wrongLeters=[...game.wrongLeters,leter]
+        if(game.estate < 6){
+            game.estate++
+            updateEstateGame(game.estate)
         }
-        if(!game.wrongLeters.includes(leter)){
-            game.wrongLeters=[...game.wrongLeters,leter]
-            return game
-        }
+            
+        return 
+    }
                 
-        return game
+        return 
+}
+        
+function createSpaceWord(winWords){
+    let placeLettersWin = document.querySelectorAll('.check-word > span')
+    if(placeLettersWin.length > 0){
+        placeLettersWin.forEach(e=> e.remove())
+         
     }
         
-    function createSpaceWord(winWords){
-        for(let leter of winWords){
-            let span= document.createElement('span')
-            span.classList.add('check-word')
-            htmlElemnts.listWinLeters[0].appendChild(span)
-            
-        }
-
+    for(let leter of winWords){
+        let span= document.createElement('span')
+        // span.classList.add('check-word')
+        htmlElemnts.listWinLeters[0].appendChild(span)
+                
     }
     
+        
+   
+}
     
-
-    updateEstateGame()
-    // console.log("🚀 ~ file: app.js ~ line 33 ~ htmlElemnts", htmlElemnts.listWinLeters[0])
-    console.log("🚀 ~ file: app.js ~ line 34 ~ game", game)
-    console.log("🚀 ~ file: app.js ~ line 35 ~ choiceRandomWord ~ choiceRandomWord", choiceRandomWord())
-    console.log("🚀 ~ file: app.js ~ line 36 ~ createWord ~ createWord", createSpaceWord(game.winWord))
-    // console.log(document.querySelectorAll('.check-word > span'))
-    // console.log("🚀 ~ file: app.js ~ line 27 ~ checkWord ~ checkWord", updateLeters('A'))
     
+   
+function  placeLetters(winLeters,wrongLeters,leterPress){
+    
+    let placeLettersWin = document.querySelectorAll('.check-word > span')
+    let lettersWrong=[...document.querySelectorAll('.wrong-word > span')].map(e=>e.innerHTML)
 
-function keyEvent(event) {
-    var key = event.keyCode || event.which;
-    var keychar = String.fromCharCode(key);
-    let onlyLeters=new RegExp('[A-Z]', 'g');
-    // console.log(onlyLeters.test(keychar))
-    if(keychar!= game.keyPress && onlyLeters.test(keychar)){
-        game.keyPress=keychar
+    if(!lettersWrong.includes(leterPress) && !game.winLeters.filter(e => e.leter === leterPress).length > 0){
+        let creatPacleLeter= document.createElement('span')
 
-        updateLeters(keychar)
-        console.log(keychar,typeof keychar)
-        console.log(game)
+        creatPacleLeter.textContent=wrongLeters.slice(-1)[0]
+        htmlElemnts.listWrongLeters[0].appendChild(creatPacleLeter)
+    }
 
+    for(let j=0; j < winLeters.length; j++){
+        placeLettersWin[winLeters[j].i].textContent=winLeters[j].leter
     }
    
-
-  
+   return
 }
+function keyEvent(event) {
+    if(game.estate < 6 && game.winLeters.length !== game.winWord.length  ){    
+        const key = event.keyCode || event.which;
+        const keychar = String.fromCharCode(key);
+        const onlyLeters=new RegExp('[A-Z]', 'g');
+        // console.log(onlyLeters.test(keychar))
+        if(keychar!= game.keyPress && onlyLeters.test(keychar)){
+            game.keyPress=keychar
+
+            updateLeters(keychar)
+            placeLetters(game.winLeters,game.wrongLeters,keychar)
+            console.log(keychar,typeof keychar)
+            console.log(game)
+        }   
+    }
+}
+function newGaem(){
+    location.reload()
+}
+updateEstateGame()
+console.log(game)
+choiceRandomWord()
+
